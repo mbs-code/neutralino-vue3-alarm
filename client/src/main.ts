ba0edb1d1a46103918da133a5db9fed9e552a5c4
mbs-code <mbscodec@gmail.com>
2022-03-13 11:18:24 +0900

    add: neu のテンプレートを移植

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,14 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-declare global {
-  interface Window {
-    Neutralino: any
-  }
-}
-
 // vue の展開
 createApp(App).mount('#app')
 
 // neu の展開
-window.Neutralino.init();
+require('./neu.ts')
